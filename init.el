0a0e586bdbfd2c680f88f02e55e9861740e177ae
byboating <byboating@gmail.com>
2015-10-13 10:14:01 +0800

    update company dash org
    update company, dash and org to lastest version using MELPA.

--- init.el
+++ init.el
@@ -127,3 +127,15 @@
     (mapc 'kill-buffer 
           (delq (current-buffer) 
                 (remove-if-not 'buffer-file-name (buffer-list)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (wsd-mode dash company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
