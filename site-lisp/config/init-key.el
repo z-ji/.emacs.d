3c54252c8b68515509ab7f904155e34212cb033b
byboating <byboating@gmail.com>
2015-10-22 11:12:38 +0800

    helm key config
    make TAB works in terminal

--- site-lisp/config/init-key.el
+++ site-lisp/config/init-key.el
@@ -40,6 +40,7 @@
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 (global-set-key (kbd "C-h r") 'helm-info-emacs)
 ;;C-<DEL>
