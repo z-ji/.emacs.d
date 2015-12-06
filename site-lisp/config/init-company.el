c4aef8bbbe4c12e6ef146ea5fcb1e3ae7421887a
byboating <byboating@gmail.com>
2015-12-06 12:08:28 +0800

    enhance auto completion
    from http://blog.binchen.org/posts/emacs-auto-completion-for-non-programmers.html

--- site-lisp/config/init-company.el
+++ site-lisp/config/init-company.el
@@ -2,4 +2,29 @@
 ;;(setq global-company-mode 1)
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
+;; "text-mode" is a major mode for editing files of text in a human language"
+;; most major modes for non-programmers inherit from text-mode
+(defun text-mode-hook-setup ()
+  ;; make `company-backends' local is critcal
+  ;; or else, you will have completion in every major mode, that's very annoying!
+  (make-local-variable 'company-backends)
+
+  ;; company-ispell is the plugin to complete words
+  (add-to-list 'company-backends 'company-ispell)
+
+  ;; OPTIONAL, if `company-ispell-dictionary' is nil, `ispell-complete-word-dict' is used
+  ;;  but I prefer hard code the dictionary path. That's more portable.
+  (setq company-ispell-dictionary (file-truename "~/.emacs.d/resources/english-words.txt")))
+
+(add-hook 'text-mode-hook 'text-mode-hook-setup)
+
+(defun toggle-company-ispell ()
+  (interactive)
+  (cond
+   ((memq 'company-ispell company-backends)
+    (setq company-backends (delete 'company-ispell company-backends))
+    (message "company-ispell disabled"))
+   (t
+    (add-to-list 'company-backends 'company-ispell)
+    (message "company-ispell enabled!"))))
 (provide 'init-company)
