7e0dcaa72421ed55b4b6dc7ce67a5fe039bf2cca
zhouji <zhouji.name@gmail.com>
2015-12-11 18:16:51 +0800

    add meo-theme

--- site-lisp/config/init-theme.el
+++ site-lisp/config/init-theme.el
@@ -13,4 +13,21 @@
 emacs-font-size)))
       (set-fontset-font (frame-parameter nil 'font) 'unicode (eval
 							      emacs-font-name))))
+(add-to-list 'custom-theme-load-path "~/.emacs.d/site-lisp/extensions/meo-theme.el/")
+;;(add-to-list 'load-path "~/.emacs.d/site-lisp/extensions/meo-theme.el/")
+(require 'moe-theme)
+;; Show highlighted buffer-id as decoration. (Default: nil)
+(setq moe-theme-highlight-buffer-id t)
+
+;; Resize titles (optional).
+;;(setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
+(setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
+(setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
+
+;; Choose a color for mode-line.(Default: blue)
+;;(moe-theme-set-color 'cyan)
+
+;;(setq moe-light-pure-white-background-in-terminal t)
+(moe-dark)
+
 (provide 'init-theme)
