51c8de2894f8cbffc701329f75ac571bd482b6a6
byboating <byboating@gmail.com>
2015-12-17 17:25:28 +0800

    optimize theme

--- site-lisp/config/init-theme.el
+++ site-lisp/config/init-theme.el
@@ -6,35 +6,35 @@
 ;;   (quote
 ;;    ("/home/byboating/.emacs.d/site-lisp/theme/emacs-color-theme-solarized/color-theme-solarized-20150813" custom-theme-directory t)))
 ;;(load-theme 'solarized t)
-;;字体设置
+;;---字体设置---
 (if (display-grayscale-p)
     (progn
       (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval
 emacs-font-size)))
       (set-fontset-font (frame-parameter nil 'font) 'unicode (eval
 							      emacs-font-name))))
-(add-to-list 'custom-theme-load-path "~/.emacs.d/site-lisp/extensions/meo-theme.el/")
 ;;(add-to-list 'load-path "~/.emacs.d/site-lisp/extensions/meo-theme.el/")
-(require 'moe-theme)
-;; Show highlighted buffer-id as decoration. (Default: nil)
-(setq moe-theme-highlight-buffer-id t)
 
-;; Resize titles (optional).
-;;(setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
-(setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
-(setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
+;;---moe-theme---
+;; (require 'moe-theme)
+;; ;; Show highlighted buffer-id as decoration. (Default: nil)
+;; (setq moe-theme-highlight-buffer-id nil)
+;; ;; Resize titles (optional).
+;; ;;(setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
+;; ;;(setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
+;; ;;(setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
+;; ;; Choose a color for mode-line.(Default: blue)
+;; (moe-theme-set-color 'w/b)
+;; ;;(setq moe-light-pure-white-background-in-terminal t)
+;; ;;(moe-dark)
+;; (require 'moe-theme-switcher)
 
-;; Choose a color for mode-line.(Default: blue)
-(moe-theme-set-color 'w/b)
-
-;;(setq moe-light-pure-white-background-in-terminal t)
-;;(moe-dark)
-(require 'moe-theme-switcher)
-;;config mode-line to powerline
+;;---powerline---
 (require 'powerline)
-(powerline-moe-theme)
+(powerline-default-theme)
+;;(powerline-moe-theme)
 
-;;Paren
+;;---paren---
 ;;启用并修改括号自带配置
 (show-paren-mode t)
 (setq show-paren-style 'expression)
@@ -46,4 +46,7 @@ emacs-font-size)))
 ;;   (lambda ()
 ;;     (highlight-parentheses-mode t)))
 (global-highlight-parentheses-mode t)
+
+;;---theme---
+;;(require 'theme)
 (provide 'init-theme)
