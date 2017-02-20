a7459dd60ef245eb5605e5af421af90d389a5d12
zhouji <zhouji.name@gmail.com>
2017-02-20 18:03:59 +0800

    Update multi-term from ELPA

--- init.el
+++ init.el
@@ -179,7 +179,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (imenu-anywhere highlight-parentheses moe-theme powerline editorconfig expand-region ox-latex-chinese org json-mode ox-twbs company dash magit))))
+    (multi-term imenu-anywhere highlight-parentheses moe-theme powerline editorconfig expand-region ox-latex-chinese org json-mode ox-twbs company dash magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
