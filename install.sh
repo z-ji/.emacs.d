4c79bac2c3200a0a87f287e1c73facd7a5d47be0
zhouji <zhouji.name@gmail.com>
2017-02-21 15:23:36 +0800

    Fixed install shell

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-git clone --recursive https://github.com/ji-zhou/.emacs.d.git ~/.emacs.d &
-sleep 1
-cd ~/.emacs.d/site-lisp/extensions/benchmark-init-el & make
+git clone --recursive https://github.com/ji-zhou/.emacs.d.git ~/.emacs.d &&
+cd ~/.emacs.d/site-lisp/extensions/benchmark-init-el && make
