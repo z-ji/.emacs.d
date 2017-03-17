05b8757af254dd1e6590d1f6c550042eec9e4f16
zhouji <zhouji.name@gmail.com>
2017-03-17 18:35:25 +0800

    Globally enable fuzzy matching
    see https://github.com/emacs-helm/helm/wiki/Fuzzy-matching

--- site-lisp/config/init-helm.el
+++ site-lisp/config/init-helm.el
@@ -3,14 +3,8 @@
 (helm-mode 1)
 (helm-autoresize-mode 1)
 
-(setq helm-M-x-fuzzy-match t
-      helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match t
-      helm-semantic-fuzzy-match t
-      helm-imenu-fuzzy-match t
-      helm-locate-fuzzy-match t
-      helm-apropos-fuzzy-match t
-      helm-lisp-fuzzy-completion t
+(setq helm-mode-fuzzy-match t
+      helm-completion-in-region-fuzzy-match t
       enable-recursive-minibuffers t)
 (setq helm-buffer-max-length 70)
 
