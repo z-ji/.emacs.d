3f24180c8263a1a435f1d2e457d86b69aa27edfd
zhouji <zhouji.name@gmail.com>
2017-03-31 18:59:47 +0800

    Upgrade packages

--- elpa/ox-latex-chinese-20160928.2153/ox-latex-chinese-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ox-latex-chinese" "20160928.2153" "An example org latex exporter configure for Chinese users" '((emacs "24.4") (cl-lib "0.5")) :url "https://github.com/tumashu/ox-latex-chinese")
