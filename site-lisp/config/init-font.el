47bc1bf9be62e4d4bbbfab2d54f1fdc0d4ee1e93
zhouji <zhouji.name@gmail.com>
2017-09-04 18:00:11 +0800

    Remove frame setting in init-font and focus on font configuration

--- site-lisp/config/init-font.el
+++ site-lisp/config/init-font.el
@@ -6,6 +6,6 @@
 ;;---setting font for emacsclient -c some_files, will also affect emacs main frame, and make font configuration above invalid---
 (setq default-frame-alist '((font . "Monaco-14")
 ;;(setq default-frame-alist '((font . "Noto Sans Mono CJK sc Regular-14")
-                            (width . 160) (height . 40)
+                            ;;(width . 160) (height . 40)
                             ))
 (provide 'init-font)
