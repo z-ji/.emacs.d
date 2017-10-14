99b3c7c52c2a589dc32f8ccb55541290d62a1b18
zhouji <zhouji.name@gmail.com>
2017-10-15 01:55:08 +0800

    Fix conflict between set-mark-command and expand-region

--- site-lisp/config/init-expand-region.el
+++ site-lisp/config/init-expand-region.el
@@ -1,4 +1,6 @@
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
 (global-set-key (kbd "C-M-w") 'er/expand-region)
+;; see https://github.com/magnars/expand-region.el/issues/220
+(setq shift-select-mode nil)
 (provide 'init-expand-region)
