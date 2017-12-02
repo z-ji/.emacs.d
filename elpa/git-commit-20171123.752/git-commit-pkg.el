02edc42c98be8faa7a5a60e0484e4a15d90bda80
zhouji <zhouji.name@gmail.com>
2017-12-03 02:22:24 +0800

    Update packages and install nlinum from GNU ELPA

---
+++ elpa/git-commit-20171123.752/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20171123.752" "Edit Git commit messages" '((emacs "24.4") (dash "20170810") (with-editor "20170817")) :commit "67234c1619b94442679aa887571f94a8477b2edc" :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
