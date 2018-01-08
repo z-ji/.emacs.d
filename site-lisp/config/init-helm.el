fb8b67fa66e56ad806554db769e25e989918b1dc
zhouji <zhouji.name@gmail.com>
2018-01-08 17:01:59 +0800

    Helm fuzzy matching

--- site-lisp/config/init-helm.el
+++ site-lisp/config/init-helm.el
@@ -1,12 +1,23 @@
-;;-----------------helm---------------------
 (require 'helm-config)
 (helm-mode 1)
 (helm-autoresize-mode 1)
-
-(setq helm-mode-fuzzy-match t
+(setq ;;;Fuzzy matching, see https://github.com/emacs-helm/helm/wiki/Fuzzy-matching
+      helm-mode-fuzzy-match t
       helm-completion-in-region-fuzzy-match t
-      enable-recursive-minibuffers t)
-(setq helm-buffer-max-length 70)
-
+      helm-recentf-fuzzy-match t
+      helm-buffers-fuzzy-matching t
+      helm-locate-fuzzy-match t
+      helm-M-x-fuzzy-match t
+      helm-semantic-fuzzy-match t
+      helm-imenu-fuzzy-match t
+      helm-apropos-fuzzy-match t
+      helm-lisp-fuzzy-completion t
+      helm-session-fuzzy-match t
+      helm-etags-fuzzy-match t
+      helm-lisp-fuzzy-completion t
+      ;;;Making the minibuffer recursive
+      enable-recursive-minibuffers t
+      ;;;Max length of buffer names
+      helm-buffer-max-length 70)
 (setq helm-external-programs-associations (quote (("pdf" . "open"))))
 (provide 'init-helm)
