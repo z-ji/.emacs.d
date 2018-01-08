1c0b98433a787b3ff4e86f0c79bec85743782880
zhouji <zhouji.name@gmail.com>
2018-01-08 15:24:34 +0800

    Update packages

---
+++ elpa/git-commit-20180107.1020/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20180107.1020" "Edit Git commit messages" '((emacs "24.4") (dash "20170810") (with-editor "20170817")) :commit "53eeafcdcb12f9deb74548ffa5b17c397ad7def2" :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
