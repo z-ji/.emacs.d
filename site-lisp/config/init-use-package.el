504cc4aeb82d1e2d45e57a6f495d00c7b77bd719
zhouji <zhouji.name@gmail.com>
2018-03-08 09:33:01 +0800

    Bindkey C-/ to C-S-/ because of conflict with undo-tree
    undo-tree-mode will not be enabled if the default undo keybindings
    (C-/ and C-_) have been overridden.

--- site-lisp/config/init-use-package.el
+++ site-lisp/config/init-use-package.el
@@ -1,6 +1,6 @@
 (bind-key* "<C-return>" 'org-insert-now)
 (bind-key* "<C-M-return>" 'org-insert-now-second)
-(bind-key* "C-/" 'comment-or-uncomment-region)
+(bind-key* "C-S-/" 'comment-or-uncomment-region)
 
 ;;;TODO only in org-mode
 (defun org-timer-start-keymap-hint()
@@ -10,3 +10,4 @@
 (bind-key* "C-c C-x 0" 'org-timer-start-keymap-hint)
 (bind-key* "C-c C-x C-0" 'org-timer-start)
 (provide 'init-use-package)
+
