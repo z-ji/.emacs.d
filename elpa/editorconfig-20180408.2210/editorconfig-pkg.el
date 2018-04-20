aea12bad99511c21a4d92d4e4e5e4e007f5f45af
zhouji <zhouji.name@gmail.com>
2018-04-20 19:28:14 +0800

    update packages

---
+++ elpa/editorconfig-20180408.2210/editorconfig-pkg.el
@@ -0,0 +1,6 @@
+(define-package "editorconfig" "20180408.2210" "EditorConfig Emacs Plugin"
+  '((cl-lib "0.5"))
+  :url "https://github.com/editorconfig/editorconfig-emacs#readme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/git-commit-20180329.1311/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20180329.1311" "Edit Git commit messages" '((emacs "24.4") (dash "20170810") (with-editor "20180318")) :commit "80fb5cdcb5cb3d54b8fd6b0a205a4facfdbc137d" :keywords '("git" "tools" "vc") :url "https://github.com/magit/magit")
