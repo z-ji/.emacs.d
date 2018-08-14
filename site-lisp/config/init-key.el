f578dfa30f1ef63f18f6dc318e5aa6cd8a764ccc
ji.zhou <zhouji.name@gmail.com>
2018-08-14 10:35:28 +0800

    bind key C-z to undo-tree-undo

--- site-lisp/config/init-key.el
+++ site-lisp/config/init-key.el
@@ -33,7 +33,7 @@
 ;;make C-z do nothing
 (global-unset-key (kbd "C-x C-z"));;unbound keys to suspend-frame
 (global-set-key [(f5)] 'speedbar)
-(global-set-key (kbd "C-z") 'undo)
+(global-set-key (kbd "C-z") 'undo-tree-undo)
 
 (global-set-key (kbd "C-S-z") 'undo-tree-redo)
 
