f00403dddb8d8bc6af8ebc1d286019b90b4e5f7f
ji.zhou <zhouji.name@gmail.com>
2018-08-20 21:18:49 +0800

    update packages

--- elpa/plantuml-mode-20180811.953/plantuml-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "plantuml-mode" "20180811.953" "Major mode for PlantUML" 'nil :commit "ecc9b14af76940083a84ca2d229a8f7bc5c4051e" :keywords '("uml" "plantuml" "ascii") :authors '(("Zhang Weize (zwz)")) :maintainer '("Carlo Sciolla (skuro)"))
