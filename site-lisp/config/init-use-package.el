d22922e988e9821b0614609ec0d5a43cbef100e3
ji.zhou <zhouji.name@gmail.com>
2018-10-14 21:50:01 +0800

    add markdown-mode

--- site-lisp/config/init-use-package.el
+++ site-lisp/config/init-use-package.el
@@ -19,4 +19,12 @@
 ;;   :config
 ;;   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
+;; markdown-mode
+(use-package markdown-mode
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 (provide 'init-use-package)
