51572be40cc1cb98187f637c0d6a18873726c873
ji.zhou <zhouji.name@gmail.com>
2018-11-08 17:34:45 +0800

    change font to Menlo-14

--- site-lisp/config/init-font.el
+++ site-lisp/config/init-font.el
@@ -6,9 +6,16 @@
 ;;---setting font for emacsclient -c some_files, will also affect emacs main frame, and make font configuration above invalid---
 
 ;;;default font for macOS
-(setq default-frame-alist '((font . "Menlo-16")
+
+;;;frame-notice-user-settings: Invalid font: format, "%s-%s", (eval emacs-font-name), (eval emacs-font-size)
+;;(setq default-frame-alist '((font . (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))))
+
+(setq default-frame-alist '((font . "Menlo-14")
 ;;(setq default-frame-alist '((font . "Monaco-16")
 ;;(setq default-frame-alist '((font . "Noto Sans Mono CJK sc Regular-14")
                             ;;(width . 160) (height . 40)
                             ))
+
+;;(add-to-list 'default-frame-alist '(font . "Menlo-14"))
+
 (provide 'init-font)
