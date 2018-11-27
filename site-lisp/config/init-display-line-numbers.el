3c50e5934f708d8d2d4d58409a6e2efa8d8ba4b9
ji.zhou <zhouji.name@gmail.com>
2018-11-28 00:02:15 +0800

    align the width of native line number

--- site-lisp/config/init-display-line-numbers.el
+++ site-lisp/config/init-display-line-numbers.el
@@ -1,3 +1,6 @@
 (global-display-line-numbers-mode t)
+(setq display-line-numbers-width-start t)
+;;(setq display-line-numbers-grow-only t)
 ;;(setq display-line-numbers 'relative)
+
 (provide 'init-display-line-numbers)
