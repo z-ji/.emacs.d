0d53df6529be6c61c9084b13874443c9a21a66ed
ji.zhou <zhouji.name@gmail.com>
2019-01-23 23:52:42 +0800

    add customize functions to sort headlines

--- site-lisp/extensions/customized-functions.el
+++ site-lisp/extensions/customized-functions.el
@@ -57,4 +57,30 @@
         (delq (current-buffer)
               (remove-if-not 'buffer-file-name (buffer-list)))))
 
+;; customize functions to sort headlines , see https://emacs.stackexchange.com/questions/9585/org-how-to-sort-headings-by-todo-and-then-by-priority
+(require 'cl)
+(require 'dash)
+
+(defun todo-to-int (todo)
+    (first (-non-nil
+            (mapcar (lambda (keywords)
+                      (let ((todo-seq
+                             (-map (lambda (x) (first (split-string  x "(")))
+                                   (rest keywords)))) 
+                        (cl-position-if (lambda (x) (string= x todo)) todo-seq)))
+                    org-todo-keywords))))
+
+(defun my/org-sort-key ()
+  (let* ((todo-max (apply #'max (mapcar #'length org-todo-keywords)))
+         (todo (org-entry-get (point) "TODO"))
+         (todo-int (if todo (todo-to-int todo) todo-max))
+         (priority (org-entry-get (point) "PRIORITY"))
+         (priority-int (if priority (string-to-char priority) org-default-priority)))
+    (format "%03d %03d" todo-int priority-int)
+    ))
+
+(defun my/org-sort-entries ()
+  (interactive)
+  (org-sort-entries nil ?f #'my/org-sort-key))
+
 (provide 'customized-functions)
