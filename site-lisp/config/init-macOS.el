9ecac8af78672057f531f1f18c03ec2439bf525c
ji.zhou <ji.zhou@chyeth.com>
2019-04-23 16:04:46 +0800

    fix: GUI display problem in macOS after upgrade to Emacs 26.2

--- site-lisp/config/init-macOS.el
+++ site-lisp/config/init-macOS.el
@@ -1,13 +1,18 @@
-;;;Command key act as Meta
+;; Command key act as Meta
 (setq mac-option-key-is-meta nil
       mac-command-key-is-meta t
       mac-command-modifier 'meta
       mac-option-modifier 'none)
-;;;other mac os settings, consider using 'emacsclient -c'
+;; other macOS settings, consider using 'emacsclient -c'
 (setq ns-pop-up-frames nil)
-;;解决中文输入法光标向前闪烁的问题
-(setq redisplay-dont-pause nil)
-;;Avoid CMD + H conflict in Emacs Mac Port, see https://emacs.stackexchange.com/questions/14823/how-to-disable-command-h-hide-in-emacs-mac-port-railwaycat
+
+;;----------------------------------------------------------------------------
+;; 可以解决中文输入法光标向前闪烁的问题
+;; commment because of GUI display problem, see https://youtu.be/BLBjGvUgOKs
+;; (setq redisplay-dont-pause nil)
+;;----------------------------------------------------------------------------
+
+;; Avoid CMD + H conflict in Emacs Mac Port, see https://emacs.stackexchange.com/questions/14823/how-to-disable-command-h-hide-in-emacs-mac-port-railwaycat
 (setq mac-pass-command-to-system nil)
 
 ;; fix error message in macOS :ls does not support --dired; see `dired-use-ls-dired' for more details.
