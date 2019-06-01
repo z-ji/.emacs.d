957e355bfdf96cf83cdc2da0c6d858e4f0dc7666
ji.zhou <ji.zhou@chyeth.com>
2019-06-01 13:00:36 +0800

    update packages

---
+++ elpa/nord-theme-20190509.652/nord-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "nord-theme" "20190509.652" "An arctic, north-bluish clean and elegant theme" '((emacs "24")) :commit "9ed7b9c2d1bb68ae86d06f97d215d2883e4ff0d2" :authors '(("Arctic Ice Studio" . "development@arcticicestudio.com")) :maintainer '("Arctic Ice Studio" . "development@arcticicestudio.com") :url "https://github.com/arcticicestudio/nord-emacs")
