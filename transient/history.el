a58770e62e6578b38860df98964c72d0aedd879f
ji.zhou <ji.zhou@chyeth.com>
2019-07-01 00:40:42 +0800

    update packages

--- transient/history.el
+++ transient/history.el
@@ -1,5 +1,12 @@
-((magit-commit nil)
+((magit-branch nil)
+ (magit-commit nil)
+ (magit-dispatch nil)
  (magit-fetch nil)
  (magit-log
   ("-n256" "--graph" "--decorate"))
- (magit-push nil))
+ (magit-push nil
+             ("--force"))
+ (magit-rebase nil)
+ (magit-remote
+  ("-f"))
+ (magit-reset nil))
