b01c9ac98fe6579630ab08ab0ca02d17834e6848
ji.zhou <ji.zhou@chyeth.com>
2019-08-09 16:45:43 +0800

    update packages

---
+++ elpa/git-commit-20190717.29/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190717.29" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "5385e81f98abed2757720479e10f77a6021af28a" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
