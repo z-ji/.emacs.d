326023e246b4a7ccbf24727e516163ecf2f7cc89
ji.zhou <ji.zhou@chyeth.com>
2019-10-03 19:36:17 +0800

    update packages

--- elpa/plantuml-mode-20190903.2113/plantuml-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "plantuml-mode" "20190903.2113" "Major mode for PlantUML" '((dash "2.0.0") (emacs "25.0")) :commit "7a1d7ab1ca2de42e37695fcc75a786125184b9d3" :keywords '("uml" "plantuml" "ascii") :authors '(("Zhang Weize (zwz)")) :maintainer '("Carlo Sciolla (skuro)"))
---
+++ elpa/plantuml-mode-20190905.838/plantuml-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "plantuml-mode" "20190905.838" "Major mode for PlantUML" '((dash "2.0.0") (emacs "25.0")) :commit "1590a75da6c3e25b726bc5e3e12656faab968917" :keywords '("uml" "plantuml" "ascii") :authors '(("Zhang Weize (zwz)")) :maintainer '("Carlo Sciolla (skuro)"))
