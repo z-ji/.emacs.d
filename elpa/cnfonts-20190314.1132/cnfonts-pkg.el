316813bfc4353cd8564009733511c87a1863c94d
ji.zhou <ji.zhou@chyeth.com>
2019-11-09 19:02:10 +0800

    add cnfonts

---
+++ elpa/cnfonts-20190314.1132/cnfonts-pkg.el
@@ -0,0 +1,12 @@
+(define-package "cnfonts" "20190314.1132" "A simple Chinese fonts config tool"
+  '((emacs "24"))
+  :keywords
+  '("convenience" "chinese" "font")
+  :authors
+  '(("Feng Shu" . "tumashu@163.com"))
+  :maintainer
+  '("Feng Shu" . "tumashu@163.com")
+  :url "https://github.com/tumashu/cnfonts")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- init.el
+++ init.el
@@ -39,7 +39,8 @@
 (require 'init-highlight-parentheses)
 ;;(require 'init-nord-theme)
 (require 'init-moe-theme)
-(require 'init-font)
+(require 'init-cnfonts)
+;;(require 'init-font)
 (require 'init-async)
 (require 'init-helm)
 (require 'init-company)
@@ -81,7 +82,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (dockerfile-mode markdown-mode exec-path-from-shell ox-pandoc restclient yaml-mode nord-theme htmlize nlinum elfeed with-editor helm-ag async use-package plantuml-mode clipmon helm multi-term imenu-anywhere highlight-parentheses moe-theme powerline editorconfig expand-region org ox-twbs company dash magit)))
+    (cnfonts dockerfile-mode markdown-mode exec-path-from-shell ox-pandoc restclient yaml-mode nord-theme htmlize nlinum elfeed with-editor helm-ag async use-package plantuml-mode clipmon helm multi-term imenu-anywhere highlight-parentheses moe-theme powerline editorconfig expand-region org ox-twbs company dash magit)))
  '(tramp-syntax (quote default) nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
