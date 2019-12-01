e5c829809d5a40ad92c32269d8df67d992f772ed
ji.zhou <ji.zhou@chyeth.com>
2019-12-01 18:29:43 +0800

    udpate packages

---
+++ elpa/helm-core-20191201.633/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20191201.633" "Development files for Helm"
+  '((emacs "24.4")
+    (async "1.9.3"))
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/multi-term-20191020.218/multi-term-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "multi-term" "20191020.218" "Managing multiple terminal buffers in Emacs." 'nil :commit "59f54c4680f62b37a19587f20b7d81da10faa146" :keywords '("term" "terminal" "multiple buffer") :authors '(("Andy Stewart" . "lazycat.manatee@gmail.com")) :maintainer '("Andy Stewart" . "lazycat.manatee@gmail.com") :url "http://www.emacswiki.org/emacs/download/multi-term.el")
