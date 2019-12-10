e9405d9a5692ea5d3b352de2a74d3173846f1510
ji.zhou <ji.zhou@chyeth.com>
2019-12-10 11:27:55 +0800

    update moe-them: mode-line color and auto dark/light
    1. using random mode-line color
    2. automatically switch between moe-dark and moe-light according to the system time

--- site-lisp/config/init-moe-theme.el
+++ site-lisp/config/init-moe-theme.el
@@ -1,8 +1,15 @@
 (require 'powerline)
 (require 'moe-theme)
 (powerline-moe-theme)
-(moe-theme-set-color 'w/b)
+
+;;(moe-theme-set-color 'w/b)
+(moe-theme-random-color)
+
 ;;(setq moe-light-pure-white-background-in-terminal t)
-(moe-dark)
+
+(require 'moe-theme-switcher)
+;;(moe-dark)
+;;(moe-light)
+;;(moe-theme-auto-switch)
 
 (provide 'init-moe-theme)
