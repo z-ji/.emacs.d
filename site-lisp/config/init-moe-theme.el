319a26d93e44e99bb951c75704c3c31aaaee1082
ji.zhou <ji.zhou@chyeth.com>
2019-12-10 14:31:09 +0800

    udpate and enable moe-theme again

--- site-lisp/config/init-moe-theme.el
+++ site-lisp/config/init-moe-theme.el
@@ -1,15 +1,22 @@
 (require 'powerline)
+
+;; Moe-theme
+(add-to-list 'custom-theme-load-path "~/.emacs.d/resources/moe-theme/")
+(add-to-list 'load-path "~/.emacs.d/resources/moe-theme/")
 (require 'moe-theme)
 (powerline-moe-theme)
 
-;;(moe-theme-set-color 'w/b)
-(moe-theme-random-color)
+;; Show highlighted buffer-id as decoration. (Default: nil)
+;;(setq moe-theme-highlight-buffer-id t)
+
+(moe-theme-set-color 'w/b)
+;;(moe-theme-random-color)
 
 ;;(setq moe-light-pure-white-background-in-terminal t)
 
-(require 'moe-theme-switcher)
-;;(moe-dark)
+(moe-dark)
 ;;(moe-light)
+;;(require 'moe-theme-switcher)
 ;;(moe-theme-auto-switch)
 
 (provide 'init-moe-theme)
