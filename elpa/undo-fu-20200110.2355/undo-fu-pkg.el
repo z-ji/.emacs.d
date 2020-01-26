5b94a8c78204212fcb1fb690dc95402a49018421
ji.zhou <ji.zhou@chyeth.com>
2020-01-26 17:27:56 +0800

    update packages

--- elpa/undo-fu-20200110.2355/undo-fu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/zj/.emacs.d/elpa/undo-fu-20200110.2355/undo-fu.el  -*- no-byte-compile: t -*-
-(define-package "undo-fu" "20200110.2355" "Undo helper with redo" '((emacs "24.3")) :commit "c5bb7b0402ea6d16b760afffec9d08f78a0b692d" :authors '(("Campbell Barton" . "ideasman42@gmail.com")) :maintainer '("Campbell Barton" . "ideasman42@gmail.com") :url "https://gitlab.com/ideasman42/emacs-undo-fu")
---
+++ elpa/undo-fu-20200118.1604/undo-fu-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "undo-fu" "20200118.1604" "Undo helper with redo" '((emacs "24.3")) :commit "61d6cda0a09e017e52693c66a2a5c8bb82a553b4" :authors '(("Campbell Barton" . "ideasman42@gmail.com")) :maintainer '("Campbell Barton" . "ideasman42@gmail.com") :url "https://gitlab.com/ideasman42/emacs-undo-fu")
