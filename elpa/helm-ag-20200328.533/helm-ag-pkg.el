668dd495ffca1285271a2b07cc37be422ece185a
ji.zhou <ji.zhou@chyeth.com>
2020-04-01 11:42:14 +0800

    update packages

---
+++ elpa/helm-ag-20200328.533/helm-ag-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-ag" "20200328.533" "the silver searcher with helm interface" '((emacs "24.4") (helm "2.0")) :commit "ad3ef038584007fbf1b7d6e727be1f18e39a8730" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-helm-ag")
--- elpa/undo-fu-20200229.2346/undo-fu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "undo-fu" "20200229.2346" "Undo helper with redo" '((emacs "24.3")) :commit "8b0b289bbd83fac489baee3f46be2225a868e0f4" :authors '(("Campbell Barton" . "ideasman42@gmail.com")) :maintainer '("Campbell Barton" . "ideasman42@gmail.com") :url "https://gitlab.com/ideasman42/emacs-undo-fu")
