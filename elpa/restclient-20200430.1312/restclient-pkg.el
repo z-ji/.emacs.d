724d51db41504b3b5ac465880134ad02e85354bc
ji0230 <zhouji.name@gmail.com>
2020-05-01 15:41:52 +0800

    update packages

---
+++ elpa/restclient-20200430.1312/restclient-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "restclient" "20200430.1312" "An interactive HTTP client for Emacs" 'nil :commit "e4f1c9824b7bc9b8041709d2914de2850806d59c" :keywords '("http") :authors '(("Pavel Kurnosov" . "pashky@gmail.com")) :maintainer '("Pavel Kurnosov" . "pashky@gmail.com"))
