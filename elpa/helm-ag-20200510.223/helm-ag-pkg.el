766b5051ef85274faecce1fdb424cb051f249eb3
ji0230 <zhouji.name@gmail.com>
2020-05-12 13:05:54 +0800

    update packages

---
+++ elpa/helm-ag-20200510.223/helm-ag-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-ag" "20200510.223" "the silver searcher with helm interface" '((emacs "25.1") (helm "2.0")) :commit "429d3d9d8d90d3b7e8d35032c42206e930f9e481" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-helm-ag")
