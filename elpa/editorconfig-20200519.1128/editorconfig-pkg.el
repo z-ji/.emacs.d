611a390823d0dafb0ad248e787605af1ff8c3e75
ji.zhou <ji.zhou@chyeth.com>
2020-06-01 00:22:42 +0800

    update packages

---
+++ elpa/editorconfig-20200519.1128/editorconfig-pkg.el
@@ -0,0 +1,11 @@
+(define-package "editorconfig" "20200519.1128" "EditorConfig Emacs Plugin"
+  '((cl-lib "0.5")
+    (emacs "24"))
+  :commit "9a73ff743b9fe820301ea9b5c5c7804bfc967324" :authors
+  '(("EditorConfig Team" . "editorconfig@googlegroups.com"))
+  :maintainer
+  '("EditorConfig Team" . "editorconfig@googlegroups.com")
+  :url "https://github.com/editorconfig/editorconfig-emacs#readme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/helm-ag-20200528.1420/helm-ag-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/zj/.emacs.d/elpa/helm-ag-20200528.1420/helm-ag.el  -*- no-byte-compile: t -*-
+(define-package "helm-ag" "20200528.1420" "The silver searcher with helm interface" '((emacs "25.1") (helm "2.0")) :commit "f18b08986c5fd9c330f513d2554ef54e0d4080d7" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-helm-ag")
