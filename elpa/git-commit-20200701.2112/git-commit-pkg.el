f61cf1b34a76b356286e0da78ad2b5450b4fe58c
Ji Zhou <zhouji.name@gmail.com>
2020-07-31 23:01:57 +0800

    update packages

---
+++ elpa/git-commit-20200701.2112/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20200701.2112" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :commit "fccd3cff52fc5b655bbb80bc6c894a2a27f188ea" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
---
+++ elpa/helm-20200731.649/helm-pkg.el
@@ -0,0 +1,9 @@
+(define-package "helm" "20200731.649" "Helm is an Emacs incremental and narrowing framework"
+  '((emacs "25.1")
+    (async "1.9.4")
+    (popup "0.5.3")
+    (helm-core "3.6.2"))
+  :commit "0e77797ef5ce89663b02d9ab12ba25fa95b9074f" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
