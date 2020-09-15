020a396287c8e7a14e5ba82f5ca3098c7dc38222
Ji Zhou <zhouji.name@gmail.com>
2020-09-15 11:44:22 +0800

    update packages

---
+++ elpa/evil-nerd-commenter-20200904.813/evil-nerd-commenter-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil-nerd-commenter" "20200904.813" "Comment/uncomment lines efficiently. Like Nerd Commenter in Vim"
+  '((emacs "24.4"))
+  :commit "b670f69b646693b50645760ee3b12bd1b9eba46b" :keywords
+  '("convenience" "evil")
+  :authors
+  '(("Chen Bin <chenbin DOT sh AT gmail.com>"))
+  :maintainer
+  '("Chen Bin <chenbin DOT sh AT gmail.com>")
+  :url "http://github.com/redguardtoo/evil-nerd-commenter")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/helm-core-20200901.741/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20200901.741" "Development files for Helm"
-  '((emacs "25.1")
-    (async "1.9.4"))
-  :commit "f89b3b974dd04e390bde2fd7da5ddff6d2f1b110" :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
