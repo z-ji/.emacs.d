2913c93b24ed9cfd31b2155dcfef509414375f15
Ji Zhou <zhouji.name@gmail.com>
2021-08-08 16:05:07 +0800

    update packages

--- elpa/highlight-parentheses-20210104.1625/highlight-parentheses-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from highlight-parentheses.el  -*- no-byte-compile: t -*-
-(define-package "highlight-parentheses" "20210104.1625" "Highlight surrounding parentheses" '((emacs "24.3") (cl-lib "0.6.1")) :commit "013ac29970d5209c14da0af6c5bb9bbbcca40c02" :authors '(("Nikolaj Schumacher <bugs * nschum de>")) :maintainer '("Tassilo Horn" . "tsdh@gnu.org") :keywords '("faces" "matching") :url "https://github.com/tsdh/highlight-parentheses.el")
--- elpa/moe-theme-20200930.853/moe-theme-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "moe-theme" "20200930.853" "A colorful eye-candy theme. Moe, moe, kyun!" 'nil :commit "76116f6eeb93c7902042f275a496629393730e08" :url "https://github.com/kuanyui/moe-theme.el")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
