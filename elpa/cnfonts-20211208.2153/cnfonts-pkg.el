344395819fe72ef2200df95202355a0e2146c7dc
Ji Zhou <zhouji.name@gmail.com>
2021-12-10 17:22:04 +0800

    update packages

---
+++ elpa/cnfonts-20211208.2153/cnfonts-pkg.el
@@ -0,0 +1,12 @@
+(define-package "cnfonts" "20211208.2153" "A simple Chinese fonts config tool"
+  '((emacs "24"))
+  :commit "2f14a3c169896f5bfe92a0bf7a76d5ebf480eb6a" :authors
+  '(("Feng Shu" . "tumashu@163.com"))
+  :maintainer
+  '("Feng Shu" . "tumashu@163.com")
+  :keywords
+  '("convenience" "chinese" "font")
+  :url "https://github.com/tumashu/cnfonts")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
