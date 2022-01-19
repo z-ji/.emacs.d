52ad209e75dbbc49b27e251ab6db76c59b35cc5e
Ji Zhou <zhouji.name@gmail.com>
2022-01-19 18:02:17 +0800

    update packages

---
+++ elpa/solidity-mode-20220106.1055/solidity-mode-pkg.el
@@ -0,0 +1,9 @@
+(define-package "solidity-mode" "20220106.1055" "Major mode for ethereum's solidity language" 'nil :commit "f0f68b038c5edf16c85fc8ca58537e1c6479738b" :authors
+  '(("Lefteris Karapetsas " . "lefteris@refu.co"))
+  :maintainer
+  '("Lefteris Karapetsas " . "lefteris@refu.co")
+  :keywords
+  '("languages" "solidity"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
