3f639e5c10a00df53694aee8763f04f6859875c0
Ji Zhou <zhouji.name@gmail.com>
2022-02-16 13:46:05 +0800

    update packages

---
+++ elpa/transient-20220130.1941/transient-pkg.el
@@ -0,0 +1,12 @@
+(define-package "transient" "20220130.1941" "Transient commands"
+  '((emacs "25.1"))
+  :commit "1f1e37709726fc945ecd03336223247a0595e811" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("bindings")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
