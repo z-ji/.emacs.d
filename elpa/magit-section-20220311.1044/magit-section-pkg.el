d3ac56abb046269083bb73bfaf56ab281be7ade4
Ji Zhou <zhouji.name@gmail.com>
2022-03-24 15:47:02 +0800

    update packages

---
+++ elpa/magit-section-20220311.1044/magit-section-pkg.el
@@ -0,0 +1,13 @@
+(define-package "magit-section" "20220311.1044" "Sections for read-only buffers"
+  '((emacs "25.1")
+    (dash "20210826"))
+  :commit "b32521d5432b5fbc33a2e47b72f2258bf98cb8b4" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/solidity-mode-20220308.1517/solidity-mode-pkg.el
@@ -0,0 +1,9 @@
+(define-package "solidity-mode" "20220308.1517" "Major mode for ethereum's solidity language" 'nil :commit "20fb77e089e10187b37ae1a94153017b82ed2a0a" :authors
+  '(("Lefteris Karapetsas " . "lefteris@refu.co"))
+  :maintainer
+  '("Lefteris Karapetsas " . "lefteris@refu.co")
+  :keywords
+  '("languages" "solidity"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
